6b0bbe310100071c426150d161570bbd5dcbb2b4
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2012-01-12 13:14:21 -0500

    Forgot to add renamed file (hotel-tracker > hoteltracker) Added pyc files to .gitignore

---
+++ hoteltracker.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2
+import sys
+try:
+    import argparse2
+except ImportError as e:
+    print(e)
+    print("Shutting down application")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    sys.exit(main())
