eaac52cbef3ddea5d7b5707d75eb8335f1a28c30
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2012-01-13 12:41:22 -0500

    Added basic functionality to check Doubletree hotel website for Anime North 2012. Writes the results to two files (p1.html and p2.html) "p2.html" lists the output of visiting the page, but it looks ugly and isn't relaly helpful. Next step is probably letting the user know if rooms are available (instead of writing the output), and how many rooms (if it is possible to compute that)

--- hoteltracker.py
+++ hoteltracker.py
@@ -1,13 +1,73 @@
 #!/usr/bin/python2
 import sys
-try:
-    import argparse2
-except ImportError as e:
-    print(e)
-    print("Shutting down application")
+import urllib
+import urllib2
+import cookielib
+
+def create_url_opener(cookie_jar = None):
+    if cookie_jar is None:
+        cookie_jar = cookielib.CookieJar()
+
+    url_opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
+    return url_opener
+
+def visit_page(url_opener, url, data={}):
+    if type(data) is dict:
+        data = urllib.urlencode(data)
+
+    #TODO: handle open errors
+    response = url_opener.open(url, data)
+    results  = response.read()
+    response.close()
+    return results
+
+def write_output(filename, content):
+    output_file = open(filename, "w");
+    output_file.write(content)
+    output_file.close()
 
 def main():
-    pass
+    doubletree_hotel_url  = "https://secure3.hilton.com/en_US/dt/reservation/book.htm"
+    doubletree_hotel_url2 = "https://secure3.hilton.com/en_US/dt/reservation/book.htm?execution=e1s1"
+
+    check_availability_data = {
+        "ctyhocn": "YYZIPDT",                                       #UNKNOWN: City Hotel Code?
+    }
+
+    check_availability_data2 = {
+        "arrivalDate": "25 May 2012",
+        "departureDate": "27 May 2012",
+        "_flexibleDates": "on",
+        "_rewardBooking": "on",
+        "numberOfRooms": "1",
+        "numberOfAdults[0]": "4",
+        "numberOfChildren[0]": "0",
+        "numberOfAdults[1]": "1",
+        "numberOfChildren[1]": "0",
+        "numberOfAdults[2]": "1",
+        "numberOfChildren[2]": "0",
+        "numberOfAdults[3]": "1",
+        "numberOfChildren[3]": "0",
+        "promoCode": "",
+        "srpId": "",
+        "onlineValueRate": "",
+        "groupCode": "AME",
+        "corporateId": "",
+        "_rememberCorporateId": "on",
+        "_aaaRate": "on",
+        "_aarpRate": "on",
+        "_governmentRate": "on",
+        "_travelAgentRate": "on",
+        "_eventId_findRoom": "Continue",
+        "execution": "e5s2"
+    }
+
+    opener    = create_url_opener()
+    #is there a way to avoid visiting this page? Its only purpose seems to be getting the process started
+    response  = visit_page(opener, doubletree_hotel_url, check_availability_data)
+    write_output("p1.html", response)
+    response2 = visit_page(opener, doubletree_hotel_url2, check_availability_data2)
+    write_output("p2.html", response2)
 
 if __name__ == "__main__":
     sys.exit(main())
