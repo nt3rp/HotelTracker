77636b9793fd8233f8feb7e6a6afaf0445490165
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2013-03-07 23:37:35 -0500

    Made function more versatile

--- hoteltracker/utils.py
+++ hoteltracker/utils.py
@@ -1,2 +1,8 @@
-def list_missing_args(required=(), provided=()):
-    return set(required) - set(provided)
+def list_missing_args(required=(), provided=(), message=None):
+    difference = set(required) - set(provided)
+
+    if not message:
+        return difference
+
+    return message.format(arguments=', '.join(difference),
+        s='s'[len(difference)==1:])
--- test/test_utils.py
+++ test/test_utils.py
@@ -24,3 +24,18 @@ class TestUtils(unittest.TestCase):
         expected = set('c')
         actual = list_missing_args(required=('a','b','c'), provided=('a','b'))
         self.assertEqual(expected, actual)
+
+    def test_list_required_argument_with_message(self):
+        expected = "Missing argument: c"
+        actual = list_missing_args(required=('a','b','c'),
+            provided=('a','b'),
+            message='Missing argument{s}: {arguments}')
+        self.assertEqual(expected, actual)
+
+    def test_list_required_argument_with_plural_message(self):
+        expected = "Missing arguments: a, c"
+        actual = list_missing_args(required=('a','b','c'),
+            provided='b',
+            message='Missing argument{s}: {arguments}')
+        self.assertEqual(expected, actual)
+
