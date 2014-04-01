0ee5b16546116bc416d1f260244cbe5d6046aded
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2014-03-31 22:53:50 -0400

    'Fixed' problem and added Radisson to script

--- an2014.py
+++ an2014.py
@@ -7,18 +7,31 @@ from twisted.internet import reactor
 # TODO: Refactor such that this class is runnable and only requires data.
 # Maybe use the custom command from one of the SO links as an example?
 
+# Why is this so difficult? Why isn't a bunch of this functionality exposed
+# right out of the box? I guess that's what `scrapyd` is for, but it is also
+# a bit of a pain in the ass (lacks scheduling, same level of documentation).
+
 # http://stackoverflow.com/a/17003736/165988
 close_signals = 0
-check_in = '2014-05-02'
-check_out = '2014-05-04'
+check_in = '2014-05-23'
+check_out = '2014-05-25'
 spider_list = [
     ('HolidayInn', {
         'location_code': 'toronto/yyzae',
         'check_in': check_in,
         'check_out': check_out,
+        'group_code': 'ANN'
+    }),
+    ('Radisson', {
+        'location_code': ' toronto-hotel-on-m9w1j1/ontorair',
+        'check_in': check_in,
+        'check_out': check_out,
+        'group_code': 'ANIME'
     })
 ]
 spiders = len(spider_list)
+
+# There should be no need to edit below this line.
 
 def stop_reactor():
     global close_signals
@@ -34,8 +47,6 @@ def setup_crawler(name, **config):
     crawler = Crawler(settings)
     crawler.configure()
 
-    # Create will pass kwargs...
-    # ... how can we automatically do that?
     spider = crawler.spiders.create(name, **config)
     crawler.crawl(spider)
     crawler.start()
